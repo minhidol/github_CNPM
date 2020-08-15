0e9eb59a88168bbf951e6efd871e75907ba279ca
18126025 <minhnn1403@gmail.com>
2020-08-15 23:25:11 +0700

    server-db

---
+++ controllers/film.controllers.js
@@ -0,0 +1,23 @@
+var Film = require('../models/film.models')
+
+const homePage = (req,res)=>
+{
+    Film.find().then(function(films)
+    {
+        res.render('acceuil',{film:films});
+    })
+}
+const detail_Film = (req,res)=>
+{
+    Film.find().then(function(films)
+    {
+        var id = req.params.id;
+        var _film = films.find(x=>x.id===id);
+        res.render('detail',{film:_film})
+    })
+}
+
+module.exports = {
+    homePage,
+    detail_Film
+};
